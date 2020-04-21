32cdd19c6f7ca9fb7cf07a4c6e7fc51dd9737210
Nate Powers <russellsproutz@gmail.com>
2020-04-21 10:37:02 -0700

    basic build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-
+import Qlogo from './Qlogo.png'
+import { Navbar, Nav, NavDropdown, Col, Row, Tab } from 'react-bootstrap';
 function App() {
+
+  var logoStyles ={
+    display: 'block',
+     marginLeft: 'auto',
+     marginRight: 'auto',
+     marginTop: 'auto',
+     marginBottom: 'auto',
+     justifyContent: 'center',
+     width: '100%',
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={Qlogo} style={logoStyles} alt="logo" />
+      
+    <header>
+  <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+    <Row>
+
+      <Col sm={3}>
+        <Nav className="flex-column">
+          <Nav.Item>
+            <Nav.Link eventKey="first">Menu</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="second">Pictures</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Col>
+      <Col sm={9}>
+        <Tab.Content>
+          <Tab.Pane eventKey="first">
+           menu
+          </Tab.Pane>
+          <Tab.Pane eventKey="second">
+          pictures
+          </Tab.Pane>
+        </Tab.Content>
+      </Col>
+    </Row>
+  </Tab.Container>
+    </header>
     </div>
   );
 }
